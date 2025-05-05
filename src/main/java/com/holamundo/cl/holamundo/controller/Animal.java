d7782d2fe29ca8c259a6c818f3ef0b09914d7867
Giuseppe <gmarcel.saavedra@gmail.com>
2025-05-05 19:15:18 -0400

    Creacion de Controlador

--- src/main/java/com/holamundo/cl/holamundo/controller/Animal.java
+++
@@ -1,7 +0,0 @@
-package com.holamundo.cl.holamundo.controller;
-
-public class Animal {
-    public void hacerSonido(){
-        System.out.println("El animal hace un sonido");
-    }
-}
--- src/main/java/com/holamundo/cl/holamundo/controller/HolaMundoController.java
+++ src/main/java/com/holamundo/cl/holamundo/controller/HolaMundoController.java
@@ -1,6 +1,5 @@
 package com.holamundo.cl.holamundo.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +9,6 @@ public class HolaMundoController{
     @RequestMapping("/app")
     public String holaMundo() {
         return "Hola a todos, mi nombre es Pedro";
-    }
-
-
-    @RequestMapping("/api/hello/{nombre}")
-    public String saludar(@PathVariable String nombre){
-        return "Hola a todos mi nombre es " + nombre;
     }
 
 }
